c4ddd3f784d29613265414f2d72c1f151848f5be
VictoriaVazhinskaya <cucumber.fridrihuya@mail.ru>
2015-03-12 23:12:19 +0300

    storing added

--- chat/scripts.js
+++ chat/scripts.js
@@ -1,8 +1,23 @@
-var userNameGlobal = 'Username';
+var userNameGlobal = '';
 var messagesForDeleting = [];
 var messageCounter = 0;
 
+var uniqueId = function() {
+	var date = Date.now();
+	var random = Math.random() * Math.random();
 
+	return Math.floor(date * random).toString();
+};
+
+var singleMessage = function ( message){
+	return {
+        sender: userNameGlobal,
+        sent: message,
+        id: uniqueId()
+	};
+};
+
+var correspondenceList = [];
 function run(){
 	var buttonLogin = document.getElementsByClassName('login')[0];
     var buttonChange = document.getElementsByClassName('change-name')[0];
@@ -10,6 +25,10 @@ function run(){
     var messagesList = document.getElementById('correspondence');
     var deleteButton = document.getElementsByClassName('delete')[0];
     var editButton = document.getElementsByClassName('edit')[0];
+    userNameGlobal = restore() || "username";
+    setUserName();
+    var allMessages = restoreMsg() || [];
+    recreateAllMessages(allMessages);
  	buttonLogin.addEventListener('click', onLoginButtonClick);
  	buttonChange.addEventListener('click', onChangeButtonClick);
     buttonSend.addEventListener('click', onSendButtonClick);
@@ -18,7 +37,16 @@ function run(){
     editButton.addEventListener('click', onEditButtonClick);
 }
 
-	
+function setUserName(){
+	document.getElementsByClassName("current-user")[0].innerHTML = userNameGlobal;
+}
+
+function recreateAllMessages(allMessages){
+	if(allMessages.length > 0){
+	for(var i=0; i<allMessages.length; i++)
+		addNewMessage(allMessages[i]);
+    }
+}	
 
 function onLoginButtonClick(){
 	var userName = document.getElementById('name-field');
@@ -28,7 +56,7 @@ function onLoginButtonClick(){
 	}
 	userNameGlobal = userName.value;
 	document.getElementsByClassName("current-user")[0].innerHTML = userName.value;
-	
+	store();
 	userName.value = "";
 	
 } 
@@ -41,7 +69,7 @@ function onChangeButtonClick(){
 	}
 	userNameGlobal = userName.value;
 	document.getElementsByClassName("current-user")[0].innerHTML = userName.value;
-	
+	store();
 	userName.value = "";
 	
 } 
@@ -50,27 +78,40 @@ function onSendButtonClick(){
 	messageCounter++;
 	var userName = document.getElementsByClassName('current-user')[0];
 	var message =  document.getElementsByClassName('write-message')[0];
+	var newMessage = singleMessage(message.value);
 	if(!message.value){
 		return;
 	}
+	addNewMessage(newMessage);
+	storeMsg(correspondenceList);
+	message.value = "";
+}
+
+function addNewMessage(msg){
+	var message = createMessage(msg);
+	correspondenceList.push(msg);
+	document.getElementById('correspondence').getElementsByTagName('ul')[0].appendChild(message);
+}
+
+function createMessage(msg){
 	var itemDiv = document.createElement('div');
-	var sender = document.createElement('li');
-	var text = document.createTextNode(userNameGlobal);
+	var senderLi = document.createElement('li');
+	var text = document.createTextNode(msg.sender);
 	itemDiv.classList.add('item');
-	itemDiv.setAttribute('id', 'mess'+ messageCounter);
-	sender.appendChild(text);
-	sender.setAttribute('id', 'user1');
-	var sent = document.createElement('li');
-	text = document.createTextNode(message.value);
-	sent.appendChild(text);
-	sent.setAttribute('id', 'message');
+	itemDiv.setAttribute('id', msg.id);
+	senderLi.appendChild(text);
+	senderLi.setAttribute('id', 'user1');
+	var sentLi = document.createElement('li');
+	text = document.createTextNode(msg.sent);
+	sentLi.appendChild(text);
+	sentLi.setAttribute('id', 'message');
 	var editBox = document.createElement('dialog');
 	editBox.classList.add('edit-message-box');
 	var closeDButton = document.createElement('input');
 	closeDButton.classList.add('close-dialog');
 	closeDButton.setAttribute('type', 'button');
 	closeDButton.setAttribute('value', 'x');
-	var textForEditting = document.createTextNode(message.value);
+	var textForEditting = document.createTextNode(msg.sent);
 	var editText = document.createElement('textarea');
 	editText.classList.add('edit-textarea');
 	editText.style.backgroundColor = 'LightSkyBlue';
@@ -82,12 +123,10 @@ function onSendButtonClick(){
 	editBox.appendChild(closeDButton);
 	editBox.appendChild(editText);
 	editBox.appendChild(submitButton);
-	itemDiv.appendChild(sender);
-	itemDiv.appendChild(sent);
+	itemDiv.appendChild(senderLi);
+	itemDiv.appendChild(sentLi);
 	itemDiv.appendChild(editBox);
-	document.getElementById('correspondence').getElementsByTagName('ul')[0].appendChild(itemDiv);
-	message.value = "";
-	setCaretPosition(document.getElementsByClassName('write-message')[0].id, 10);
+	return itemDiv;
 }
 
 function emit(evtObj){
@@ -119,6 +158,15 @@ function onDeleteButtonClick(){
     	var childElement = document.getElementById(messagesForDeleting[i]);
     	parentElement.removeChild(childElement);
     }
+    for(var i=0; i<messagesForDeleting.length; i++){   	
+    	for(var k=0; k<correspondenceList.length; k++){
+    		if( messagesForDeleting[i] == correspondenceList[k].id){
+    		correspondenceList.splice(k, 1);
+    		break;
+    	    }
+    	}
+    }
+    storeMsg(correspondenceList);
     messagesForDeleting.splice(0, messagesForDeleting.length);
     if(messagesForDeleting.length == 0){
         document.getElementsByClassName('edit')[0].style.backgroundColor = 'black';
@@ -159,14 +207,20 @@ function onEditInsightDBoxButtonClick(evtObj){
 	
 		var dialogBox = evtObj.target.parentElement;
 		var oldMessage =  dialogBox.parentElement;
-		var newMessage = dialogBox.getElementsByTagName('textarea')[0];
+		var newMessage = dialogBox.getElementsByTagName('textarea')[0];       
         if(!newMessage.value){
         	return;
         }
-
+        var messageId = oldMessage.id;
+        for(var i=0; i<correspondenceList.length; i++){
+            if(correspondenceList[i].id == messageId){
+            	correspondenceList[i].sent = newMessage.value;
+                break;
+            }
+        }
+        storeMsg(correspondenceList);
         oldMessage.getElementsByTagName('li')[1].innerHTML = newMessage.value;
         oldMessage.getElementsByTagName('dialog')[0].close();
-	    
 }
 
 function setCaretPosition(elemId, caretPos) {
@@ -187,4 +241,48 @@ function setCaretPosition(elemId, caretPos) {
                 elem.focus();
         }
     }
+}
+
+
+function store() {
+
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	localStorage.setItem("current user", userNameGlobal);
+}
+
+function storeMsg(listToSave) {
+	
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	localStorage.setItem("correspondence", JSON.stringify(listToSave));
+}
+
+function restore() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	var item = localStorage.getItem("current user");
+
+	return item;
+}
+
+
+function restoreMsg() {
+	if(typeof(Storage) == "undefined") {
+		alert('localStorage is not accessible');
+		return;
+	}
+
+	var item = localStorage.getItem("correspondence");
+
+	return item && JSON.parse(item);
 }
